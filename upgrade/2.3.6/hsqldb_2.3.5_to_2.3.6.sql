5c2cca808497800a80cdcfc66505b1d844078469
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-01-26 15:48:38 +0000

    delete folder upgrade/2.3.6

--- upgrade/2.3.6/hsqldb_2.3.5_to_2.3.6.sql
+++
@@ -1,9 +0,0 @@
---
--- Table structure for table 'jforum_spam'
---
-CREATE TABLE jforum_spam (
-  pattern VARCHAR(100) NOT NULL
-);
-
-ALTER TABLE jforum_topics ALTER COLUMN topic_title VARCHAR(120);
-ALTER TABLE jforum_posts_text ALTER COLUMN post_subject VARCHAR(130);
