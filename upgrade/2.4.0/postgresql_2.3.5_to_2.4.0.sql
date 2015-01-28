b596bb75e17a1f3c552ff9abf2b0004e41a1deed
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-01-28 09:03:45 +0000

    expand topic_title and post_subject columns

--- upgrade/2.4.0/postgresql_2.3.5_to_2.4.0.sql
+++ upgrade/2.4.0/postgresql_2.3.5_to_2.4.0.sql
@@ -2,9 +2,12 @@
 -- Table structure for table 'jforum_spam'
 --
 CREATE TABLE jforum_spam (
-  pattern varchar(100) NOT NULL
+  pattern VARCHAR(100) NOT NULL
 );
+
+-- more characters for topic title
+ALTER TABLE jforum_topics ALTER COLUMN topic_title TYPE VARCHAR(120);
+ALTER TABLE jforum_posts_text ALTER COLUMN post_subject TYPE VARCHAR(130);
 
 -- more characters for more secure hash
 ALTER TABLE jforum_users ALTER COLUMN user_password TYPE VARCHAR(128);
-
