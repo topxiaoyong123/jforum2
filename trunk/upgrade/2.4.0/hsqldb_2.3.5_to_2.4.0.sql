2e6cdeb6a158c349519a46098737e63df28fa258
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-01-28 09:03:45 +0000

    expand topic_title and post_subject columns

--- trunk/upgrade/2.4.0/hsqldb_2.3.5_to_2.4.0.sql
+++ trunk/upgrade/2.4.0/hsqldb_2.3.5_to_2.4.0.sql
@@ -5,6 +5,9 @@ CREATE TABLE jforum_spam (
   pattern VARCHAR(100) NOT NULL
 );
 
+-- more characters for topic title
+ALTER TABLE jforum_topics ALTER COLUMN topic_title VARCHAR(120);
+ALTER TABLE jforum_posts_text ALTER COLUMN post_subject VARCHAR(130);
+
 -- more characters for more secure hash
 ALTER TABLE jforum_users ALTER COLUMN user_password VARCHAR(128);
-
--- trunk/upgrade/2.4.0/sqlserver_2.3.5_to_2.4.0.sql
+++ trunk/upgrade/2.4.0/sqlserver_2.3.5_to_2.4.0.sql
@@ -5,6 +5,9 @@ CREATE TABLE jforum_spam (
   pattern nvarchar(100) NOT NULL
 );
 
+-- more characters for topic title
+ALTER TABLE jforum_topics ALTER COLUMN topic_title nvarchar(120);
+ALTER TABLE jforum_posts_text ALTER COLUMN post_subject nvarchar(130);
+
 -- more characters for more secure hash
 ALTER TABLE jforum_users ALTER COLUMN user_password VARCHAR(128);
-
