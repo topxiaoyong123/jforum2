60ace413df8861bacfefeb8b89bcfecec5d909f7
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-01-30 12:13:06 +0000

    Rewrite to avoid eclipse warning

--- trunk/src/main/java/net/jforum/util/SortedProperties.java
+++ trunk/src/main/java/net/jforum/util/SortedProperties.java
@@ -58,14 +58,12 @@ public class SortedProperties extends Properties
 	/**
 	 * Overrides, called by the store method.
 	 */
-	@SuppressWarnings("unchecked")
-	public synchronized Enumeration keys() {
-		Enumeration keysEnum = super.keys();
-		Vector keyList = new Vector();
-		while(keysEnum.hasMoreElements()){
-			keyList.add(keysEnum.nextElement());
-		}
-		Collections.sort(keyList);
-		return keyList.elements();
-	}
+	public synchronized Enumeration<Object> keys() {
+        Vector<String> v = new Vector<String>();
+        for (Object o : keySet()) {
+            v.add(o.toString());
+        }
+        Collections.sort(v);
+        return new Vector<Object>(v).elements();
+    }
 }
