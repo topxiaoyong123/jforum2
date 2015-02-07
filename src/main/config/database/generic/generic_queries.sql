4061e2703d9778a7902fbcc6e18d6548f1dc589d
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-02-07 11:55:01 +0000

    add user_viewonline to the generic_queries.sql fix spelling error for encrypt fix CSS bug shCore.css

--- src/main/config/database/generic/generic_queries.sql
+++ src/main/config/database/generic/generic_queries.sql
@@ -213,7 +213,7 @@ PostModel.selectAllByTopicByLimit = SELECT p.post_id, topic_id, forum_id, p.user
 	LIMIT ?, ?
 
 PostModel.selectByUserByLimit = SELECT p.post_id, topic_id, forum_id, p.user_id, post_time, poster_ip, enable_bbcode, p.attach, \
-	enable_html, enable_smilies, enable_sig, post_edit_time, post_edit_count, status, pt.post_subject, pt.post_text, username, p.need_moderate \
+	enable_html, enable_smilies, enable_sig, post_edit_time, post_edit_count, status, pt.post_subject, pt.post_text, username, p.need_moderate, u.user_viewonline \
 	FROM jforum_posts p, jforum_posts_text pt, jforum_users u \
 	WHERE p.post_id = pt.post_id \
 	AND p.user_id = u.user_id \
