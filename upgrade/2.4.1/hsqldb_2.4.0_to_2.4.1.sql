fd1c7e84bcc81674668dd8b28a03738be7e1a88f
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-05-14 18:01:05 +0000

    database upgrade scripts from 2.4.0 to 2.4.1 1.drop table jforum_themes 2.remove gender and themes_id attributes from jforum_users table

---
+++ upgrade/2.4.1/hsqldb_2.4.0_to_2.4.1.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS jforum_themes;
+
+ALTER TABLE jforum_users DROP gender;
+ALTER TABLE jforum_users DROP themes_id;
---
+++ upgrade/2.4.1/postgresql_2.4.0_to_2.4.1.sql
@@ -0,0 +1,5 @@
+DROP SEQUENCE jforum_themes_seq;
+DROP TABLE jforum_themes;
+
+ALTER TABLE jforum_users DROP gender;
+ALTER TABLE jforum_users DROP themes_id;
