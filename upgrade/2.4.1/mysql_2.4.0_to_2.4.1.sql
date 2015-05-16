1ab5dc02c569d2ce2e97718159e00ca43e854753
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-05-16 18:08:09 +0000

    change the order of drop table and drop sequence after drop column

--- upgrade/2.4.1/mysql_2.4.0_to_2.4.1.sql
+++ upgrade/2.4.1/mysql_2.4.0_to_2.4.1.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS jforum_themes;
-
 ALTER TABLE jforum_users DROP gender;
 ALTER TABLE jforum_users DROP themes_id;
+
+DROP TABLE IF EXISTS jforum_themes;
--- upgrade/2.4.1/sqlserver_2.4.0_to_2.4.1.sql
+++ upgrade/2.4.1/sqlserver_2.4.0_to_2.4.1.sql
@@ -1,5 +1,5 @@
-IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'jforum_themes') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)		
-DROP TABLE jforum_themes;
-
 ALTER TABLE jforum_users DROP COLUMN gender;
 ALTER TABLE jforum_users DROP COLUMN themes_id;
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'jforum_themes') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)		
+DROP TABLE jforum_themes;
