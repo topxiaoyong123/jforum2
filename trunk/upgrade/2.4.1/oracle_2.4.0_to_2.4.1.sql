6cd6dfd36f83961cdeb1f81e92c9db69d3f0c2f5
andowson <andowson@475ff8d2-7104-a99a-79b8-54628088a72a>
2015-05-16 18:08:09 +0000

    change the order of drop table and drop sequence after drop column

--- trunk/upgrade/2.4.1/oracle_2.4.0_to_2.4.1.sql
+++ trunk/upgrade/2.4.1/oracle_2.4.0_to_2.4.1.sql
@@ -1,4 +1,4 @@
-DROP SEQUENCE jforum_themes_seq;
-DROP TABLE  jforum_themes;
-
 ALTER TABLE jforum_users DROP (gender, themes_id);
+
+DROP TABLE  jforum_themes;
+DROP SEQUENCE jforum_themes_seq;
--- trunk/upgrade/2.4.1/postgresql_2.4.0_to_2.4.1.sql
+++ trunk/upgrade/2.4.1/postgresql_2.4.0_to_2.4.1.sql
@@ -1,5 +1,5 @@
-DROP SEQUENCE jforum_themes_seq;
-DROP TABLE jforum_themes;
-
 ALTER TABLE jforum_users DROP gender;
 ALTER TABLE jforum_users DROP themes_id;
+
+DROP TABLE jforum_themes;
+DROP SEQUENCE jforum_themes_seq;
